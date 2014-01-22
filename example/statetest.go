04907155bc6ea735399891d7340820b67bd54f05
Josh Chase <jcjoshuachase@gmail.com>
2014-01-22 10:18:52 -0500

    some updates to how state machines work

--- example/statetest.go
+++ example/statetest.go
@@ -5,27 +5,53 @@ import (
 	"fmt"
 )
 
-func state1(s *state.State,i interface{}) error {
+type embeddedState struct {
+	st state.Simple
+	transitions int
+}
+
+func newES() *embeddedState {
+	es := embeddedState{}
+	return &es
+}
+
+func (e *embeddedState) Pop() (state.StateTrans,error) {
+	return e.st.Pop()
+}
+
+func (e *embeddedState) Push(states ...state.StateTrans) {
+	e.st.Push(states...)
+}
+
+func (e *embeddedState) Len() int {
+	return e.st.Len()
+}
+
+func state1(s state.StateMachine,i interface{}) error {
+	es := s.(*embeddedState)
+	es.transitions++
 	fmt.Println("State 1!")
-	s.PushFront(state2)
+	s.Push(state2)
 	return nil
 }
 
-func state2(s *state.State,i interface{}) error {
+func state2(s state.StateMachine,i interface{}) error {
+	es := s.(*embeddedState)
+	es.transitions++
 	fmt.Println("State 2!")
-	s.PushFront(state1)
 	return nil
 }
 
 func main() {
-	s := &state.State{}
-	s.PushFront(state1)
+	s := newES()
+	s.Push(state1,state2,state2)
 	for i := 0; i < 10; i++ {
-		err := s.Input(i)
+		err := state.Input(s,i)
 		fmt.Println("machine is",s.Len(),"states long")
 		if err != nil {
 			fmt.Println(err)
 		}
 	}
+	fmt.Println("Transitioned",s.transitions,"times!")
 }
 
--- state.go
+++ state.go
@@ -6,7 +6,7 @@ import (
 	"container/list"
 )
 
-type StateTrans func(*State,interface{}) error
+type StateTrans func(StateMachine,interface{}) error
 type stateErr struct {
 	Message, Info string
 }
@@ -24,24 +24,50 @@ const (
 	InvalidTrans = "Invalid Transition!"
 )
 
-func (s *State) transOne(i int) error {
+func (s *Simple) transOne(i int) error {
 	return nil
 }
 
-type State struct {
-	list.List
-	Final bool
+type StateMachine interface {
+	Push(...StateTrans)
+	Pop() (StateTrans,error)
 }
 
-func (s *State) Input(i interface{}) error {
-	front := s.Front()
-	if front != nil {
-		f,ok := front.Value.(func(*State,interface{})error)
-		if ok {
-			s.Remove(front)
-			return f(s,i)
-		}
-		return stateErr{Message: InvalidTrans,Info: fmt.Sprintf("%v",reflect.TypeOf(front.Value))}
+func Input(s StateMachine, i interface{}) error {
+	f,err := s.Pop()
+	if err == nil {
+		return f(s,i)
 	}
-	return stateErr{Message: StackEmpty}
+	return err
+}
+
+type Simple struct {
+	stack list.List
+}
+
+func New() *Simple {
+	return &Simple{}
+}
+
+func (s *Simple) Push(states ...StateTrans) {
+	for i := len(states) - 1; i >= 0; i-- {
+		s.stack.PushFront(states[i])
+	}
+}
+
+func (s *Simple) Pop() (StateTrans, error) {
+	if s.stack.Len() != 0 {
+		front := s.stack.Front()
+		f, ok := front.Value.(StateTrans)
+		if ok {
+			s.stack.Remove(front)
+			return f,nil
+		}
+		return nil,stateErr{Message: InvalidTrans, Info: fmt.Sprintf("%v",reflect.TypeOf(front.Value))}
+	}
+	return nil, stateErr{Message: StackEmpty}
+}
+
+func (s *Simple) Len() int {
+	return s.stack.Len()
 }
